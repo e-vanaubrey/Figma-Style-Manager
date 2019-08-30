89de58203e528cd23e009a5085881950512c91dc
Evan Aubrey <evanaubrey@Evans-MacBook-Pro-2.local>
2019-08-30 16:20:54 -0700

    shows styles. multi drag and drop works.

--- code.ts
+++ code.ts
@@ -8,7 +8,7 @@ figma.showUI(__html__);
 let results = [];
 let colors = [];
 for (let h = 0; h < figma.getLocalPaintStyles().length; h++) {
-  results.push(figma.getLocalPaintStyles()[h]);
+  //colors.push(figma.getLocalPaintStyles()[h].id);
   colors.push(figma.getLocalPaintStyles()[h].paints[0]);
 }
 
@@ -17,13 +17,18 @@ for (let h = 0; h < figma.getLocalPaintStyles().length; h++) {
 //figma.ui.postMessage(results);
 //figma.ui.postMessage(results);
 figma.ui.postMessage(colors);
+//figma.ui.postMessage(results);
 
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+//figma.ui.onmessage = msg => {
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+  //figma.closePlugin();
+//};
+figma.ui.onmessage = (message) => {
+  console.log("got this from the UI", message)
+  //figma.getLocalPaintStyles() = message.slice();
+}
